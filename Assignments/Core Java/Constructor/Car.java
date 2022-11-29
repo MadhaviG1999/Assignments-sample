28d9ea5381fa365f10ace773a5947e60cc13df62
MadhaviG1999 <93818459+MadhaviG1999@users.noreply.github.com>
2022-11-29 17:36:59 +0530

    Add files via upload

---
+++ Assignments/Core Java/Constructor/Car.java
@@ -0,0 +1,14 @@
+package constructor;
+
+public class Car 
+{
+	
+		  static int modelYear=1950;
+		  static String modelName="Benz";
+
+		  public Car(int year, String name) {
+		    modelYear = year;
+		    modelName = name;
+		    }
+
+}
---
+++ Assignments/Core Java/Constructor/Maincar.java
@@ -0,0 +1,12 @@
+package constructor;
+
+public class Maincar {
+
+	public static void main(String[] args) 
+	{
+		
+		    System.out.println(Car.modelYear + " " + Car.modelName);
+
+	}
+
+}
