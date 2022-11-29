ed4fe212070c7e9ffebfd3b5f95f2bf6e20897d5
MadhaviG1999 <93818459+MadhaviG1999@users.noreply.github.com>
2022-11-29 17:39:19 +0530

    Add files via upload

---
+++ Assignments/Core Java/Static/Main.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+		  // Static method
+		  static void myStaticMethod() {
+		    System.out.println("Static methods can be called without creating objects");
+		  }
+
+		  // Public method
+		  public void myPublicMethod() {
+		    System.out.println("Public methods must be called by creating objects");
+		  }
+
+}
---
+++ Assignments/Core Java/Static/Testsample.java
@@ -0,0 +1,12 @@
+
+public class Testsample
+{
+	public static void main(String[] args)
+	{
+	   
+	    Main myObj = new Main(); // Create an object of MyClass
+	    myObj.myPublicMethod(); // Call the public method
+	    myObj.myStaticMethod();
+	  }
+
+	}
